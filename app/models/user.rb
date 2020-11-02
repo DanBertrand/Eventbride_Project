47816662806ceef61d5f539e0a6a17773011f77d
Dan <dan.bertrand.hs@gmail.com>
2020-11-02 13:22:23 +0100

    5th commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
 	has_many :attendances
-	has_many :events, through :attendances
+	has_many :events, through: :attendances
+	has_many :events_administrator, foreign_key: "administrator_id", class_name: "Event"
+
+	validates :first_name, 
+		presence: true,
+
+	validates :last_name, 
+		presence: true,
+
+	validates :email, 
+		presence: true,
+		uniqueness: true,
+		format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+
+	validates :description, 
+		presence: true,
+
+	validates :encrypted_password, 
+		presence: true,
+		length: {minimum: 6}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "faker"
+
+
+User.destroy_all
+
+
+
+10.times do
+	user = User.create!(
+		description: Faker::Lorem.paragraphs(rand(2..8)).join('\n'), 
+		first_name: Faker::Name.first_name, 
+		last_name: Faker::Name.last_name,
+		email: Faker::Internet.email, 
+		encrypted_password:"password")
+end
+puts "#{tp User.all}"
+
